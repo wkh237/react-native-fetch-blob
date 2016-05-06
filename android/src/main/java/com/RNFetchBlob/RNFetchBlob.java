435dace51ded87803df3e893fe6673bd64b424f3
Ben Hsieh <xeiyan@gmail.com>
2016-05-06 16:32:17 +0800

    Add multipart support for Android

--- android/src/main/java/com/RNFetchBlob/RNFetchBlob.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlob.java
@@ -1,11 +1,13 @@
 package com.RNFetchBlob;
 
 import android.net.Uri;
+import android.text.style.AlignmentSpan;
 
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.loopj.android.http.AsyncHttpClient;
@@ -14,6 +16,7 @@ import com.loopj.android.http.Base64;
 import com.loopj.android.http.BinaryHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import java.io.ByteArrayOutputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
@@ -91,6 +94,98 @@ public class RNFetchBlob extends ReactContextBaseJavaModule {
                     break;
                 case "put" :
                     req.put(this.getReactApplicationContext(), url, entity, "octet-stream",handler);
+                    break;
+                case "delete" :
+                    req.delete(url, params, handler);
+                    break;
+            }
+        } catch(Exception error) {
+            callback.invoke( "RNFetchBlob serialize request data failed: " + error.getMessage() + error.getCause());
+        }
+
+    }
+
+    @ReactMethod
+    public void fetchBlobForm(String method, String url, ReadableMap headers, ReadableArray body, final Callback callback) {
+
+        try {
+            Uri uri = Uri.parse(url);
+            AsyncHttpClient req = new AsyncHttpClient();
+
+            // set params
+            RequestParams params = new RequestParams();
+            ByteArrayEntity entity = null;
+
+            // set params
+            for (String paramName : uri.getQueryParameterNames()) {
+                params.put(paramName, uri.getQueryParameter(paramName));
+            }
+
+            // set headers
+            ReadableMapKeySetIterator it = headers.keySetIterator();
+            while (it.hasNextKey()) {
+                String key = it.nextKey();
+                req.addHeader(key, headers.getString(key));
+            }
+
+            // set body for POST and PUT
+            if(body != null && method.equalsIgnoreCase("post") || method.equalsIgnoreCase("put")) {
+
+                Long tsLong = System.currentTimeMillis()/1000;
+                String ts = tsLong.toString();
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+                String boundary = "RNFetchBlob".concat(ts);
+                for(int i =0; i<body.size() ; i++) {
+                    ReadableMap map = body.getMap(i);
+                    String name = map.getString("name");
+                    if(map.hasKey("filename")) {
+                        String filename = map.getString("filename");
+                        byte [] file = Base64.decode(map.getString("data"), 0);
+                        outputStream.write(String.format("--%s\r\n", boundary).getBytes("UTF-8"));
+                        outputStream.write(String.format("Content-Disposition: form-data; name=\"%s\"; filename=\"%s\"\r\n", boundary, name, filename).getBytes("UTF-8"));
+                        outputStream.write(String.format("Content-Type: application/octet-stream\r\n\r\n").getBytes());
+                        outputStream.write(file);
+                        outputStream.write("\r\n".getBytes());
+                    }
+                    else {
+                        String data = map.getString("data");
+                        outputStream.write(String.format("--%s\r\n", boundary).getBytes("UTF-8"));
+                        outputStream.write(String.format("Content-Disposition: form-data; name=\"%s\"; \r\n", boundary, name).getBytes("UTF-8"));
+                        outputStream.write(String.format("Content-Type: text/plain\r\n\r\n").getBytes());
+                        outputStream.write((data+"\r\n").getBytes());
+                    }
+                }
+                outputStream.write(String.format("--%s--\r\n", boundary).getBytes());
+                byte bodyBytes[] = outputStream.toByteArray( );
+                entity = new ByteArrayEntity(bodyBytes);
+                entity.setContentType(headers.getString("Content-Type") + "; charset=utf8; boundary=" + boundary);
+                req.addHeader("Content-Type", headers.getString("Content-Type") + "; charset=utf8; boundary=" + boundary);
+            }
+
+            // create handler
+            AsyncHttpResponseHandler handler = new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] binaryData) {
+                    String value = Base64.encodeToString(binaryData, Base64.NO_WRAP);
+                    callback.invoke(null, value);
+                }
+
+                @Override
+                public void onFailure(final int statusCode, final Header[] headers, byte[] binaryData, final Throwable error) {
+                    callback.invoke(statusCode, error.getMessage()+ ", "+ error.getCause());
+                }
+            };
+
+            // send request
+            switch(method.toLowerCase()) {
+                case "get" :
+                    req.get(url, params, handler);
+                    break;
+                case "post" :
+                    req.post(this.getReactApplicationContext(), url, entity, "multipart/form-data; charset=utf8", handler);
+                    break;
+                case "put" :
+                    req.put(this.getReactApplicationContext(), url, entity, "multipart/form-data",handler);
                     break;
                 case "delete" :
                     req.delete(url, params, handler);
