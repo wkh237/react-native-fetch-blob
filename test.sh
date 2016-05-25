465484e3e29b1d5b26228eef5a9d286828ef91fa
Ben Hsieh <xeiyan@gmail.com>
2016-05-25 10:14:26 +0800

    Add development packages

--- test.sh
+++ test.sh
@@ -11,20 +11,20 @@ if [ "$#" -eq 1 ]; then
     TARGET="$1"
     echo 'start ios test'
   else
-    exit "unreconized target platform $1"
+    exit "unrecongized target platform $1, please specify a target platform `ios` or `android`."
   fi
 elif [ "$#" -gt 2 ] && [ "$1" == 'path' ]; then
   TEST_APP_PATH="$2"
   TARGET="$3"
   echo "use exist test app path=$2 target=$3"
 else
-  exit "unreconized arguments."
+  exit "unrecongized arguments."
 fi
 
 
 # Create new rn project
 if [ "$#" -eq 1 ]; then
-  echo 'create new app for test ..'
+  echo 'creating test app RNFetchBlobTest ..'
   rm -rf "${TEST_APP_NAME}"
   react-native init "${TEST_APP_NAME}"
 fi
@@ -38,12 +38,12 @@ rnpm link
 # start RN
 cd "${TEST_APP_PATH}"
 if [ "$#" == 4 ]; then
-  sed -i.bak "s/RNFetchBlobTest/$4/" ./index.test.js
+  sed -i.bak "s/${TEST_APP_NAME}/$4/" ./index.test.js
 fi
 react-native "run-${TARGET}"
 
 # start test server
-cd "${CWD}/test/test-server"
+cd "${CWD}/test-server"
 # kill existing server
 kill "$(lsof | grep :8123 | awk '{ printf $2 }')"
 node server
