54c51811826b9a83b122b6e4788bc2b7c351be32
Ben Hsieh <xeiyan@gmail.com>
2016-05-26 15:42:13 +0800

    Add rnpm script for change android permission

---
+++ src/scripts/prelink.js
@@ -0,0 +1,18 @@
+var fs = reuqire('fs');
+var MANIFEST_PATH = process.cwd() + '/android/app/src/main/AndroidManifest.xml';
+
+fs.readFile(MANIFEST_PATH, function(err, data) {
+
+  if(err)
+    console.log('failed to locate AndroidManifest.xml file, you may have to add file access permission manually.');
+  else {
+
+    data = String(data).replace(
+      '<uses-permission android:name="android.permission.INTERNET" />',
+      '<uses-permission android:name="android.permission.INTERNET" />\n  <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" /> '
+    )
+    fs.writeFileSync(MANIFEST_PATH, data);
+
+  }
+
+})
