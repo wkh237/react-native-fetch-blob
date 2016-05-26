2941dd99b846c8c0e145cf63f0208fbb3d075e27
Ben Hsieh <xeiyan@gmail.com>
2016-05-26 15:45:13 +0800

    Fix typo

--- src/scripts/prelink.js
+++ src/scripts/prelink.js
@@ -1,4 +1,4 @@
-var fs = reuqire('fs');
+var fs = require('fs');
 var MANIFEST_PATH = process.cwd() + '/android/app/src/main/AndroidManifest.xml';
 
 fs.readFile(MANIFEST_PATH, function(err, data) {
@@ -9,7 +9,7 @@ fs.readFile(MANIFEST_PATH, function(err, data) {
 
     data = String(data).replace(
       '<uses-permission android:name="android.permission.INTERNET" />',
-      '<uses-permission android:name="android.permission.INTERNET" />\n  <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" /> '
+      '<uses-permission android:name="android.permission.INTERNET" />\n    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" /> '
     )
     fs.writeFileSync(MANIFEST_PATH, data);
 
