3a001d447ad137ed8446e571da23171caba5add1
Ben Hsieh <xeiyan@gmail.com>
2016-06-01 00:27:52 +0800

    Refactor test server code

--- test-server/server.js
+++ test-server/server.js
@@ -3,87 +3,44 @@
  * @description react-native-fetch-blob test & dev server
  */
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var chokidar = require('chokidar');
-var multer = require('multer');
-var upload = multer({dest : 'uploads/'});
-var chalk = require('chalk');
-var mkdirp = require('mkdirp');
-var dirname = require('path').dirname;
-var app = express();
-var fs = require('fs');
+var express = require('express')
+var bodyParser = require('body-parser')
+var chokidar = require('chokidar')
+var multer = require('multer')
+var upload = multer({dest : 'uploads/'})
+var chalk = require('chalk')
+var mkdirp = require('mkdirp')
+var dirname = require('path').dirname
+var app = express()
+var fs = require('fs')
 
 var JS_SOURCE_PATH = '../test/',
-    APP_SOURCE_PATH = '../RNFetchBlobTest/';
+    LIB_SOURCE_PATH = '../src/',
+    NODE_MODULE_MODULE_PATH = '../RNFetchBlobTest/node_modules/react-native-fetch-blob/',
+    APP_SOURCE_PATH = '../RNFetchBlobTest/'
 
-// watch index.js in src folder
-chokidar
-  .watch('../src/index.js')
-  .on('change', function(path) {
-    console.log(chalk.green('js file changed'), path);
-    var targetPath = String(path).replace('../src/', '../RNFetchBlobTest/node_modules/react-native-fetch-blob/')
-    mkdirp(dirname(targetPath), function (err) {
-    if (err) return cb(err);
-      fs.writeFileSync(targetPath, fs.readFileSync(path));
-    });
-  })
-
-// watch files in  test folder
-chokidar
-  .watch(JS_SOURCE_PATH)
-  .on('add', function(path) {
-    console.log(chalk.magenta('file created'), path);
-    var targetPath = String(path).replace(JS_SOURCE_PATH, APP_SOURCE_PATH)
-    mkdirp(dirname(targetPath), function (err) {
-    if (err) return cb(err);
-      fs.writeFileSync(targetPath, fs.readFileSync(path));
-    });
-  })
-  .on('change', function(path) {
-    console.log(chalk.green('file changed'), path);
-    var targetPath = String(path).replace(JS_SOURCE_PATH, APP_SOURCE_PATH)
-    mkdirp(dirname(targetPath), function (err) {
-    if (err) return cb(err);
-      fs.writeFileSync(targetPath, fs.readFileSync(path));
-    });
-  })
-  .on('unlink', function(path) {
-    console.log(chalk.red('file removed'), path);
-    var targetPath = String(path).replace(JS_SOURCE_PATH, APP_SOURCE_PATH)
-    mkdirp(dirname(targetPath), function (err) {
-    if (err) return cb(err);
-      fs.unlinkSync(targetPath);
-    });
-  })
-  .on('error', function(err){
-    console.log(err);
-  });
+// watch test app source
+watch(JS_SOURCE_PATH, APP_SOURCE_PATH)
+// watch lib js source
+watch(LIB_SOURCE_PATH, NODE_MODULE_MODULE_PATH, {ignored: /\.\.\/src\/(android|ios)\//})
 
 app.listen(8123, function(err){
-
   if(!err)
     console.log('test server running at port ',8123)
-
 })
 
 
 app.use(function(req,res,next){
-
-  console.log(chalk.green('request url=') + chalk.magenta(req.url));
-
+  console.log(chalk.green('request url=') + chalk.magenta(req.url))
   next()
 })
 
 app.use(upload.any())
-
 app.use('/public', express.static('./public'))
-
 // for redirect test
 app.get('/redirect', function(req, res) {
   res.redirect('/public/github.png')
 })
-
 // handle octet-stream request
 app.post('/upload', function(req, res){
 
@@ -97,12 +54,10 @@ app.post('/upload', function(req, res){
   })
 
 })
-
 // return an empty response
 app.all('/empty', function(req, res) {
   res.send('')
 })
-
 // handle multipart/form-data request
 app.post('/upload-form', function(req, res) {
   console.log(req.headers)
@@ -119,3 +74,36 @@ app.post('/upload-form', function(req, res) {
     files : req.files
   })
 })
+
+function watch(source, dest, ignore) {
+  // watch files in  test folder
+  chokidar
+    .watch(source, ignore)
+    .on('add', function(path) {
+      console.log(chalk.magenta('file created'), path)
+      var targetPath = String(path).replace(source, dest)
+      mkdirp(dirname(targetPath), function (err) {
+      if (err) return cb(err)
+        fs.writeFileSync(targetPath, fs.readFileSync(path))
+      })
+    })
+    .on('change', function(path) {
+      console.log(chalk.green('file changed'), path)
+      var targetPath = String(path).replace(source, dest)
+      mkdirp(dirname(targetPath), function (err) {
+      if (err) return cb(err)
+        fs.writeFileSync(targetPath, fs.readFileSync(path))
+      })
+    })
+    .on('unlink', function(path) {
+      console.log(chalk.red('file removed'), path)
+      var targetPath = String(path).replace(source, dest)
+      mkdirp(dirname(targetPath), function (err) {
+      if (err) return cb(err)
+        fs.unlinkSync(targetPath)
+      })
+    })
+    .on('error', function(err){
+      console.log(err)
+    })
+}
