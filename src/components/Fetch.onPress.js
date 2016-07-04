dea4ad411adf163c5720816acb044ba84a2476cc
Ben Hsieh <xeiyan@gmail.com>
2016-07-04 21:45:28 +0800

    wip commit

---
+++ src/components/Fetch.onPress.js
@@ -0,0 +1,10 @@
+// press button to login
+{/*<Fetch.onPress method="POST"
+  url="www.example.com/login"
+  body={this.state.body}
+  onSuccess={this.onSuccess}
+  onError={this.showError}>
+  <View>
+  	<Text>Click to Login</Text>
+  </View>
+</LoginAction.onPress>*/}
---
+++ src/components/Fetch.when.js
@@ -0,0 +1,15 @@
+// placeholder
+{/*<Fetch method="GET"
+  url={`www.example.com/avatar/${this.state.userId}`}
+  onSuccess={(data) => { this.setState({avatar : data.path()}) }}
+  onError={this.showError}>
+  <Fetch.before>
+		<Image souce="images/placeholder.png"/>
+	</Fetch.before>
+  <Fetch.success>
+		<Image souce={this.state.avatar}/>
+	</Fetch.success>
+  <Fetch.error>
+		<Image souce="images/error.png"/>
+	</Fetch.error>
+</LoginAction.onPress>*/}
