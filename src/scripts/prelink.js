33409ae402540c8785fabb10cabfcd56becb70b7
Ben Hsieh <xeiyan@gmail.com>
2016-07-14 23:24:36 +0800

    Change prelink script for 0.29 compatial #53

--- src/scripts/prelink.js
+++ src/scripts/prelink.js
@@ -1,6 +1,28 @@
 var fs = require('fs');
-var MANIFEST_PATH = process.cwd() + '/android/app/src/main/AndroidManifest.xml';
 
+var MANIFEST_PATH = process.cwd() + '/android/app/src/main/AndroidManifest.xml';
+var PACKAGE_JSON = process.cwd() + '/package.json';
+
+var hasNecessaryFile = fs.existsSync(MANIFEST_PATH) && fs.existsSync(MANIFEST_PATH);
+
+if (!hasNecessaryFile) {
+  throw 'RNFetchBlob could not found link Android automatically, some files could not be found.'
+}
+
+var package = fs.readFileSync(PACKAGE_JSON);
+var APP_NAME = package.name;
+var VERSION = parseFloat(/\d\.\d+(?=\.)/.exec(package.dependencies['react-native']));
+var APPLICATION_MAIN = process.cwd() + '/android/app/src/main/java/com/' + APP_NAME.toLocaleLowerCase() + '/MainApplication.java';
+
+if(VERSION >= 0.29) {
+
+  var main = fs.readFileSync(APPLICATION_MAIN);
+  main = main.replace('new MainReactPackage()', 'new RNFetchBlobPackage(),\n           new MainReactPackage()');
+  fs.writeFileSync(APPLICATION_MAIN, main);
+
+}
+
+// set file access permission for Android < 6.0
 fs.readFile(MANIFEST_PATH, function(err, data) {
 
   if(err)
