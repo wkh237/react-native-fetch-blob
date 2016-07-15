0b5c0f29ecda5e0ba6e39621cb4c3a35d468c105
Ben Hsieh <xeiyan@gmail.com>
2016-07-15 09:12:16 +0800

    revert rnpm link script change #53

--- src/scripts/prelink.js
+++ src/scripts/prelink.js
@@ -9,26 +9,6 @@ if (!hasNecessaryFile) {
   throw 'RNFetchBlob could not found link Android automatically, some files could not be found.'
 }
 
-var package = fs.readFileSync(PACKAGE_JSON);
-var APP_NAME = package.name;
-var APPLICATION_MAIN = process.cwd() + '/android/app/src/main/java/com/' + APP_NAME.toLocaleLowerCase() + '/MainApplication.java';
-
-if(!fs.existsSync('APPLICATION_MAIN')) {
-  throw 'RNFetchBlob could not found link Android automatically, MainApplication.java not found in path : ' + APPLICATION_MAIN
-}
-
-var VERSION = checkVersion();
-console.log('RNFetchBlob detected app version .. ' + VERSION);
-
-if(VERSION >= 0.29) {
-  console.log('RNFetchBlob patching MainApplication.java .. ');
-  var main = fs.readFileSync(APPLICATION_MAIN);
-  main = main.replace('new MainReactPackage()', 'new RNFetchBlobPackage(),\n           new MainReactPackage()');
-  fs.writeFileSync(APPLICATION_MAIN, main);
-  console.log('RNFetchBlob patching MainApplication.java .. ok')
-
-}
-
 // set file access permission for Android < 6.0
 fs.readFile(MANIFEST_PATH, function(err, data) {
 
