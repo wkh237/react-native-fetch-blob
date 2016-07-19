1eef1919ec4ae56038527a526ec8fb4505f99cc1
Ben Hsieh <xeiyan@gmail.com>
2016-07-20 00:52:38 +0800

    Fix cancel request error

--- src/index.js
+++ src/index.js
@@ -109,19 +109,20 @@ function fetch(...args:any):Promise {
   // create task ID for receiving progress event
   let taskId = getUUID()
   let options = this || {}
+  let subscription, subscriptionUpload
 
   let promise = new Promise((resolve, reject) => {
     let [method, url, headers, body] = [...args]
     let nativeMethodName = Array.isArray(body) ? 'fetchBlobForm' : 'fetchBlob'
 
     // on progress event listener
-    let subscription = emitter.addListener('RNFetchBlobProgress', (e) => {
+    subscription = emitter.addListener('RNFetchBlobProgress', (e) => {
       if(e.taskId === taskId && promise.onProgress) {
         promise.onProgress(e.written, e.total)
       }
     })
 
-    let subscriptionUpload = emitter.addListener('RNFetchBlobProgress-upload', (e) => {
+    subscriptionUpload = emitter.addListener('RNFetchBlobProgress-upload', (e) => {
       if(e.taskId === taskId && promise.onUploadProgress) {
         promise.onUploadProgress(e.written, e.total)
       }
