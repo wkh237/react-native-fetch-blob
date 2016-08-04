646399f438f50f1b705c9d7689800e3d02a7982b
Ben Hsieh <xeiyan@gmail.com>
2016-08-04 14:40:41 +0800

    Change stress test code

--- test/test-0.6.3.js
+++ test/test-0.6.3.js
@@ -31,19 +31,40 @@ describe('massive HTTP request', (report, done) => {
 
   let promises = []
   let progress = []
+  let iteration = 500
   let begin = Date.now()
-  for(let i=0; i<500;i++) {
+  let finished = 0
+  let success = 0
+  for(let i=0; i<iteration;i++) {
     let p = RNFetchBlob
     .config({fileCache : true})
-    .fetch('GET', `${TEST_SERVER_URL}/public/github2.jpg`)
-    promises.push(p)
+    .fetch('GET', `${TEST_SERVER_URL}/stress/${i}`)
+    .then((res) => {
+      finished ++
+      let info = res.info()
+      if(info.status == 200 && info.headers['Content-Length'] == '23975') {
+        success ++
+      }
+      report(<Info key={`stress progress ${success}/${finished} tests successfully completed. size=${info.headers['Content-Length']} elapsed=${Date.now() - begin}ms`} uid="stress063"/>)
+      fs.unlink(res.path()).catch(() => {})
+      if(finished >= iteration)
+        summary()
+    })
+    .catch((err) => {
+      finished++
+      report(<Info key={err} uid="stress063-err"/>)
+      if(finished >= iteration){
+        summary()
+      }
+    })
   }
-  Promise.all(promises).then((resps) => {
-    for(let i in resps) {
-      fs.unlink(resps[i].path())
-    }
-    report(<Info key={`time = ${(Date.now() - begin) / 1000} sec`}></Info>)
+
+  function summary() {
+    report(
+      <Info key={`time = ${(Date.now() - begin) / 1000} sec`}>
+        <Text>{`${success} success`}</Text>
+      </Info>)
     done()
-  })
+  }
 
 })
