8c4d56ff27ae01807d4ea5abecaf10c13fa95e5c
Ben Hsieh <xeiyan@gmail.com>
2016-08-17 00:28:30 +0800

    Add IOS empty form data field checking #90

--- src/ios/RNFetchBlobReqBuilder.m
+++ src/ios/RNFetchBlobReqBuilder.m
@@ -154,11 +154,19 @@
     {
         __block int i = 0;
         __block int count = [form count];
+        // a recursive block that builds multipart body asynchornously
         void __block (^getFieldData)(id field) = ^(id field)
         {
             NSString * name = [field valueForKey:@"name"];
             NSString * content = [field valueForKey:@"data"];
             NSString * contentType = [field valueForKey:@"type"];
+            // skip when the form field `name` or `data` is empty
+            if(content == nil || name == nil)
+            {
+                i++;
+                getFieldData([form objectAtIndex:i]);
+                return;
+            }
             contentType = contentType == nil ? @"application/octet-stream" : contentType;
             // field is a text field
             if([field valueForKey:@"filename"] == nil || content == [NSNull null]) {
