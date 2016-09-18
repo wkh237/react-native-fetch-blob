483988b09931032a19ba3ac155dbb8d57064f57b
Ben Hsieh <xeiyan@gmail.com>
2016-09-18 11:34:58 +0800

    Add test cases

--- test/test-0.9.5.js
+++ test/test-0.9.5.js
@@ -28,7 +28,77 @@ const dirs = RNFetchBlob.fs.dirs
 
 let prefix = ((Platform.OS === 'android') ? 'file://' : '')
 
-describe('issue #122 force response data format', (report, done) => {
+// describe('issue #122 force response data format', (report, done) => {
+//
+//   RNFetchBlob.fetch('GET', `${TEST_SERVER_URL}/public/json-dummy.json`, {
+//     'RNFB-Response' : 'base64'
+//   })
+//   .then((res) => {
+//     let r = RNFetchBlob.base64.decode(res.data)
+//     report(
+//       <Assert key="test data verify" expect="fetchblob-dev" actual={JSON.parse(r).name}/>,
+//       <Assert key="should successfully decode the data" expect={true} actual={true}/>)
+//     return RNFetchBlob.fetch('GET', `${TEST_SERVER_URL}/public/json-dummy.json`)
+//   })
+//   .then((res) => {
+//     report(
+//       <Assert key="response should in format of plain-text" expect="fetchblob-dev" actual={JSON.parse(res.data).name}/>)
+//     done()
+//   })
+//   .catch(() => {
+//     report(
+//       <Assert key="Should successfully decode the data" expect={true} actual={false}/>)
+//     done()
+//   })
+//
+// })
+
+// describe('#129 memory leaking when enable uploadProgress', (report, done) => {
+//
+//   let file = null
+//   let count = 0
+//
+//   RNFetchBlob.config({ fileCache : true })
+//     .fetch('GET', `${TEST_SERVER_URL}/public/6mb-dummy`)
+//     .then((res) => {
+//       file = res.path()
+//       setTimeout(() => {
+//         for(let i=0;i<20;i++){
+//           RNFetchBlob.fetch('POST', `${TEST_SERVER_URL}/upload`, {}, RNFetchBlob.wrap(file))
+//           .uploadProgress(() => {})
+//           .then(() => {
+//             if(count > 20) {
+//               fs.unlink(file)
+//               report(<Assert key="finished" expect={true} actual={true}/>)
+//               done()
+//             }
+//           })
+//         }
+//       }, 3000)
+//     })
+//
+// })
+
+describe('#131 status code != 200 should not throw an error', (report, done) => {
+
+  let count = 0
+  let codes = [404, 500, 501, 403]
+
+  codes.forEach((code) => {
+
+    RNFetchBlob.fetch('GET', `${TEST_SERVER_URL}/xhr-code/${code}`, {
+      'Cache-Control' : 'no-store'
+    })
+    .then(function(res) {
+      report(<Assert key={`status code should be ${this}`} expect={Math.floor(this)} actual={Math.floor(res.info().status)}/>)
+      count ++
+      if(count >= 4)
+        done()
+    }.bind(code))
+    .catch(function(err) {
+      report(<Assert key={`status code ${this} should not cause error`} expect={true} actual={false}/>)
+      count++
+    }.bind(code))
 
   })
 
