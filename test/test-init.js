ca038bec7d510fd6a449e33329e82fc9af8d827d
Ben Hsieh <xeiyan@gmail.com>
2016-09-30 10:50:05 +0800

    Add #143 test case, test API and test data

--- test/test-init.js
+++ test/test-init.js
@@ -18,10 +18,10 @@ const { Assert, Comparer, Info, prop } = RNTest
 // test environment variables
 
 prop('FILENAME', `${Platform.OS}-0.8.0-${Date.now()}.png`)
-// prop('TEST_SERVER_URL', 'http://localhost:8123')
-// prop('TEST_SERVER_URL_SSL', 'https://localhost:8124')
-prop('TEST_SERVER_URL', 'http://192.168.17.194:8123')
-prop('TEST_SERVER_URL_SSL', 'https://192.168.17.194:8124')
+prop('TEST_SERVER_URL', 'http://localhost:8123')
+prop('TEST_SERVER_URL_SSL', 'https://localhost:8124')
+// prop('TEST_SERVER_URL', 'http://192.168.17.194:8123')
+// prop('TEST_SERVER_URL_SSL', 'https://192.168.17.194:8124')
 prop('DROPBOX_TOKEN', 'fsXcpmKPrHgAAAAAAAAAoXZhcXYWdgLpQMan6Tb_bzJ237DXhgQSev12hA-gUXt4')
 prop('styles', {
   image : {
@@ -72,7 +72,8 @@ describe('GET image from server', (report, done) => {
 // require('./test-0.9.2')
 // require('./test-0.9.4')
 // require('./test-0.9.5')
-require('./test-0.9.6')
+// require('./test-0.9.6')
+require('./test-stream')
 // require('./test-fetch')
 // require('./test-fs')
 // require('./test-xmlhttp')
