959b8f5b3fddb6398da3337c00d6b06ee1bf2504
Ben Hsieh <xeiyan@gmail.com>
2017-01-17 10:36:32 +0800

    #226 Remove flow type annotations

--- src/class/RNFetchBlobSession.js
+++ src/class/RNFetchBlobSession.js
@@ -1,7 +1,6 @@
 // Copyright 2016 wkh237@github. All rights reserved.
 // Use of this source code is governed by a MIT-style license that can be
 // found in the LICENSE file.
-// @flow
 
 import {
  NativeModules,
