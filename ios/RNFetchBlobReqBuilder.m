016a480b3ab4c25c7790273d94ae4a230460f8b4
Bronco <heybronco@gmail.com>
2017-04-24 15:17:34 +0800

    Using custom content type for text field in iOS (#340)

--- ios/RNFetchBlobReqBuilder.m
+++ ios/RNFetchBlobReqBuilder.m
@@ -201,16 +201,18 @@
                 RCTLogWarn(@"RNFetchBlob multipart request builder has found a field without `data` or `name` property, the field will be removed implicitly.");
                 return;
             }
-            contentType = contentType == nil ? @"application/octet-stream" : contentType;
+
             // field is a text field
             if([field valueForKey:@"filename"] == nil || content == nil) {
+                contentType = contentType == nil ? @"text/plain" : contentType;
                 [formData appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
                 [formData appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n", name] dataUsingEncoding:NSUTF8StringEncoding]];
-                [formData appendData:[[NSString stringWithFormat:@"Content-Type: text/plain\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+                [formData appendData:[[NSString stringWithFormat:@"Content-Type: %@\r\n\r\n", contentType] dataUsingEncoding:NSUTF8StringEncoding]];
                 [formData appendData:[[NSString stringWithFormat:@"%@\r\n", content] dataUsingEncoding:NSUTF8StringEncoding]];
             }
             // field contains a file
             else {
+                contentType = contentType == nil ? @"application/octet-stream" : contentType;
                 NSMutableData * blobData;
                 if(content != nil)
                 {
