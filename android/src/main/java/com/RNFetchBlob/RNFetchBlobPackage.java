5ed606dec95c3792383642a7f682dfd53a845981
Alberto Schiabel <jkomyno@users.noreply.github.com>
2017-09-15 15:49:50 +0200

    Deprecated createJSModules on Android only for RN >= 0.47

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobPackage.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobPackage.java
@@ -20,6 +20,7 @@ public class RNFetchBlobPackage implements ReactPackage {
         return modules;
     }
 
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
