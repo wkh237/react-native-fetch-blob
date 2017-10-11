fda87ba53a7cf0e4ad3de9a66a7b1127607eb2b4
Phodal Huang <h@phodal.com>
2017-10-11 08:57:16 -0500

    fixed getExternalFilesDir null issue

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobFS.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobFS.java
@@ -213,7 +213,12 @@ public class RNFetchBlobFS {
         state = Environment.getExternalStorageState();
         if (state.equals(Environment.MEDIA_MOUNTED)) {
             res.put("SDCardDir", Environment.getExternalStorageDirectory().getAbsolutePath());
-            res.put("SDCardApplicationDir", ctx.getExternalFilesDir(null).getParentFile().getAbsolutePath());
+            File externalFilesDir = ctx.getExternalFilesDir(null);
+            if(externalFilesDir != null) {
+                res.put("SDCardApplicationDir", externalFilesDir.getParentFile().getAbsolutePath());
+            } else {
+                res.put("SDCardApplicationDir", Environment.getExternalStorageDirectory().getAbsolutePath());
+            }
         }
         res.put("MainBundleDir", ctx.getApplicationInfo().dataDir);
         return res;
