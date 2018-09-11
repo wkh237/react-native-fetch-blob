7c20eaf1400654e7426ac93373af42af61903b76
sun qiang <sunqiangqiang@shimo.im>
2018-09-11 11:32:35 +0800

    fix:删除废弃api

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobPackage.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobPackage.java
@@ -21,11 +21,6 @@ public class RNFetchBlobPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
