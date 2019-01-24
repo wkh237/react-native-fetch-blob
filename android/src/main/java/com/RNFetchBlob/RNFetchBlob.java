e83f84f1329218c3cf52b99d7a0dbb3a1a8c47d8
Greg Alexander <gregory.l.alexander@gmail.com>
2019-01-23 23:13:52 -0600

    upgrading to sdk 28 and androidx

--- android/src/main/java/com/RNFetchBlob/RNFetchBlob.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlob.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 import android.util.SparseArray;
 
 import com.facebook.react.bridge.ActivityEventListener;
@@ -28,7 +28,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.JavaNetCookieJar;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
--- android/src/main/java/com/RNFetchBlob/Response/RNFetchBlobFileResp.java
+++ android/src/main/java/com/RNFetchBlob/Response/RNFetchBlobFileResp.java
@@ -1,7 +1,6 @@
 package com.RNFetchBlob.Response;
 
-import android.support.annotation.NonNull;
-import android.util.Log;
+import androidx.annotation.NonNull;
 
 import com.RNFetchBlob.RNFetchBlobConst;
 import com.RNFetchBlob.RNFetchBlobProgressConfig;
