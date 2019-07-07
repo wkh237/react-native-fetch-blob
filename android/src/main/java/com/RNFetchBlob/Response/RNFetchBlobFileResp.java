f836385b10f4d74691a5c4ff1fb4e8b51d9bd196
Sushant Gupta <mya.sushant@yahoo.com>
2019-07-07 15:44:59 +0530

    Bug fix for download interruption promise rejection

--- android/src/main/java/com/RNFetchBlob/Response/RNFetchBlobFileResp.java
+++ android/src/main/java/com/RNFetchBlob/Response/RNFetchBlobFileResp.java
@@ -69,6 +69,10 @@ public class RNFetchBlobFileResp extends ResponseBody {
         return originalBody.contentLength();
     }
 
+    public boolean isDownloadComplete() {
+        return bytesDownloaded == contentLength();
+    }
+
     @Override
     public BufferedSource source() {
         ProgressReportingSource countable = new ProgressReportingSource();
