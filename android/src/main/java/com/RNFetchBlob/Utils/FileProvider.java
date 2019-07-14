6153af89bb0e608b5ab4c55b2a2800a7f5565a90
Travis Nuttall <Traviskn@users.noreply.github.com>
2019-07-14 16:59:31 -0600

    use android x file provider

--- android/src/main/java/com/RNFetchBlob/Utils/FileProvider.java
+++ android/src/main/java/com/RNFetchBlob/Utils/FileProvider.java
@@ -1,4 +1,4 @@
 package com.RNFetchBlob.Utils;
 
-public class FileProvider extends android.support.v4.content.FileProvider {
+public class FileProvider extends androidx.core.content.FileProvider {
 }
