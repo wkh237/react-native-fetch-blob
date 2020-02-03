c6b372766febffacec4366a1fad214a442e5cf04
ihavenoface5 <ihavenoface@gmail.com>
2020-02-03 09:20:17 -0700

    Update RNFetchBlobReq.java
    added class cast exception for FileStorage response type.

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
@@ -591,7 +591,17 @@ public class RNFetchBlobReq extends BroadcastReceiver implements Runnable {
 //                    ignored.printStackTrace();
                 }
 
-                RNFetchBlobFileResp rnFetchBlobFileResp = (RNFetchBlobFileResp) responseBody;
+                try {
+                    RNFetchBlobFileResp rnFetchBlobFileResp = (RNFetchBlobFileResp) responseBody;
+                } catch (ClassCastException ex) {
+                    // unexpected response type
+                    if (responseBody != null) {
+                        callback.invoke("Unexpected FileStorage response file: " + responseBody.string(), null);
+                    } else {
+                        callback.invoke("Unexpected FileStorage response with no file.", null);
+                    }
+                    return;
+                }
 
                 if(rnFetchBlobFileResp != null && !rnFetchBlobFileResp.isDownloadComplete()){
                     callback.invoke("Download interrupted.", null);
